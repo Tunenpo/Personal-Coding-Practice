f012bce7d8d4d6ff209cce7b96e7a5e8b0f4aaae
Tunenpo <wahabsalam1994@gmail.com>
2023-09-05 17:15:37 +0100

    combination of printf puts & Putchar to code

---
+++ personal/combination.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("I love to code\n");
+
+	puts("I love to code");
+
+	putchar('I');
+	putchar(' ');
+	putchar('l');
+	putchar('o');
+	putchar('v');
+	putchar('e');
+	putchar(' ');
+	putchar('t');
+	putchar('o');
+	putchar(' ');
+	putchar('c');
+	putchar('o');
+	putchar('d');
+	putchar('e');
+	putchar('\n');
+
+	return (0);
+}
