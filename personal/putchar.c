8afbc57174a2dae20d565b6d2f4da10b0c0dd106
Tunenpo <wahabsalam1994@gmail.com>
2023-09-05 17:05:08 +0100

    Using putchar to print

---
+++ personal/putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	putchar('I');
+	putchar(' ');
+	putchar('L');
+	putchar('o');
+	putchar('v');
+	putchar('e');
+	putchar(' ');
+	putchar('C');
+	putchar('\n');
+
+	return (0);
+}
