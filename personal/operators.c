b9fd9281ba4b98697d1aba8e5781b8dddebf7976
Tunenpo <wahabsalam1994@gmail.com>
2023-09-06 12:30:41 +0100

    Operators in C

---
+++ personal/operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+	/* Declarattion of variables */
+	int add1;
+	int add2;
+
+	/* Initialization of variables */
+	add1 = 10;
+	add2 = 20;
+
+	/* addtition of the two variables */
+	int summation = add1 + add2;
+
+	printf("%d\n",  summation);
+
+	return (0);
+}
--- personal/wahab.c
+++ personal/wahab.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("This is the beginning of my coding practice\n");
+	printf("hello world\n");
 
 	return (0);
 }
